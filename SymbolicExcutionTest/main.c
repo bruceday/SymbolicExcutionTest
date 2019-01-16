b68f731d2432ad263da56fec9705cfea963bc448
bruceday <bruceday@foxmail.com>
2019-01-16 22:25:06 +0800

    se

--- SymbolicExcutionTest/main.c
+++ SymbolicExcutionTest/main.c
@@ -5,28 +5,29 @@
 
 void main()
 {
-		inC_RPC *inC = (inC_RPC*)malloc(sizeof(inC_RPC));
-		outC_RPC *outC = (outC_RPC*)malloc(sizeof(outC_RPC));
-		RPC_reset(outC);
-		kcg_bool IN_MNB_status;
-		kcg_bool IN_MNB_value;
-		kcg_bool I_A_status;
-		kcg_bool I_A_value;
-		kcg_real SVAL;
-		kcg_real HYST;
-		kcg_bool O_TEMP_status;
-		kcg_bool O_TEMP_value;
-		kcg_bool O_A_WARING_status;
-		kcg_bool O_A_WARING_value;
-		klee_make_symbolic(&inC->IN_MNB.status, sizeof(inC->IN_MNB.status), "IN_MNB_status");
-		klee_make_symbolic(&inC->IN_MNB.value, sizeof(inC->IN_MNB.value), "IN_MNB_value");
-		klee_make_symbolic(&inC->I_A.status, sizeof(inC->I_A.status), "I_A_status");
-		klee_make_symbolic(&inC->I_A.value, sizeof(inC->I_A.value), "I_A_value");
-		klee_make_symbolic(&inC->SVAL, sizeof(inC->SVAL), "SVAL");
-		klee_make_symbolic(&inC->HYST, sizeof(inC->HYST), "HYST");
-		klee_make_symbolic(&outC->O_TEMP.status, sizeof(outC->O_TEMP.status), "O_TEMP_status");
-		klee_make_symbolic(&outC->O_TEMP.value, sizeof(outC->O_TEMP.value), "O_TEMP_value");
-		klee_make_symbolic(&outC->O_A_WARNING.status, sizeof(outC->O_A_WARNING.status), "O_A_WARING_status");
-		klee_make_symbolic(&outC->O_A_WARNING.value, sizeof(outC->O_A_WARNING.value), "O_A_WARING_value");
-		RPC(inC,outC);
+	inC_RPC inC;
+	outC_RPC outC;
+
+	kcg_bool IN_MNB_status;
+	kcg_bool IN_MNB_value;
+	kcg_bool I_A_status;
+	kcg_bool I_A_value;
+	kcg_real SVAL;
+	kcg_real HYST;
+	kcg_bool O_TEMP_status;
+	kcg_bool O_TEMP_value;
+	kcg_bool O_A_WARING_status;
+	kcg_bool O_A_WARING_value;
+	klee_make_symbolic(&inC.IN_MNB.status, sizeof(inC.IN_MNB.status), "IN_MNB_status");
+	klee_make_symbolic(&inC.IN_MNB.value, sizeof(inC.IN_MNB.value), "IN_MNB_value");
+	klee_make_symbolic(&inC.I_A.status, sizeof(inC.I_A.status), "I_A_status");
+	klee_make_symbolic(&inC.I_A.value, sizeof(inC.I_A.value), "I_A_value");
+	klee_make_symbolic(&inC.SVAL, sizeof(inC.SVAL), "SVAL");
+	klee_make_symbolic(&inC.HYST, sizeof(inC.HYST), "HYST");
+	klee_make_symbolic(&outC.O_TEMP.status, sizeof(outC.O_TEMP.status), "O_TEMP_status");
+	klee_make_symbolic(&outC.O_TEMP.value, sizeof(outC.O_TEMP.value), "O_TEMP_value");
+	klee_make_symbolic(&outC.O_A_WARNING.status, sizeof(outC.O_A_WARNING.status), "O_A_WARING_status");
+	klee_make_symbolic(&outC.O_A_WARNING.value, sizeof(outC.O_A_WARNING.value), "O_A_WARING_value");
+	RPC_reset(&outC);
+	RPC(&inC, &outC);
 }
